a0704b61f1862909eef7582d6ead8c9d756f1319
mohankri <mohankri@gmail.com>
2017-09-05 21:24:56 -0700

    handle response

--- mynode.js
+++ mynode.js
@@ -23,23 +23,26 @@ var options = {
 
 var req = http.request(options, function (res) {
   var chunks = [];
-
+  console.log("response code " + res.statusCode);
   res.on("data", function (chunk) {
     chunks.push(chunk);
   });
 
   res.on("end", function () {
-	var body = JSON.parse(chunks);
-	body = JSON.parse(body.message);
-	console.log("TagId " + body.tagId);
-	console.log("TagId " + body["tagId"]);
+  	if (res.statusCode == 200) {
+		var body = JSON.parse(chunks);
+		body = JSON.parse(body.message);
+		console.log("TagId " + body.tagId);
+		console.log("TagId " + body["tagId"]);
+	}
 	//console.log(JSON.stringify(body));
   });
 });
 
 var object = {}
 
-var orderNumber = 3505245;
+//var orderNumber = 3505245;
+var orderNumber = "GAM145955"
 //var data =  "{" + \"tagId\" + ":" + "\"" orderNumber + "\"" + "," + \"fetchType\" + ":" + \"details\" + "}";
 object['tagId'] = orderNumber;
 object['fetchType'] = "details";
